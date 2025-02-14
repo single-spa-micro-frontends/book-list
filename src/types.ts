4d0c295729d97311875ba10df195880fe17b1031
BajicMladen <bajicmladen33@gmail.com>
2025-02-14 22:05:19 +0100

    Improve type-safety.

--- src/types.ts
+++ src/types.ts
@@ -1,41 +1,42 @@
 export type Book = {
-    id: string;
-    volumeInfo: {
-      title: string;
-      authors: string[];
-      publisher?: string;
-      publishedDate?: string;
-      description?: string;
-      pageCount?: number;
-      categories?: string[];
-      imageLinks?: {
-        smallThumbnail?: string;
-        thumbnail?: string;
-      };
-      language?: string;
-    };
-    saleInfo: {
-      listPrice:{
-        amount: number
-      }
-    }
-  }
-
-export type BookPayload = {
-    bookId: string;
+  id: string;
+  volumeInfo: {
     title: string;
-    image: string | undefined;
-    quantity: number;
-    [key: string]: any; 
-}
+    authors: string[];
+    publisher?: string;
+    publishedDate?: string;
+    description?: string;
+    pageCount?: number;
+    categories?: string[];
+    imageLinks?: {
+      smallThumbnail?: string;
+      thumbnail?: string;
+    };
+    language?: string;
+  };
+  saleInfo: {
+    listPrice: {
+      amount: number;
+    };
+  };
+};
+
+export type CartItem = {
+  bookId: string;
+  title: string;
+  image: string | undefined;
+  quantity: number;
+  [key: string]: any;
+};
 
 type EventBus = {
-  addToCart:(book: BookPayload) => void
-  setSelectedBook:(id:string) => void
+  addToCart: (book: CartItem) => void;
+  setSelectedBook: (id: string) => void;
   searchState$: {
-    subscribe: (callback: (state: { query: string }) => void) => { unsubscribe: () => void };
+    subscribe: (callback: (state: { query: string }) => void) => {
+      unsubscribe: () => void;
+    };
   };
-}
+};
 
-
-export type EnrichedWindow = Window & {eventBus : EventBus}
+export type EnrichedWindow = Window & { eventBus: EventBus };
